6274469110c141a467c2459aa9862e1d170d4b1b
Somwshwar <sonusolat512@gmail.com>
2024-03-10 22:31:16 +0530

    Inital Commit

---
+++ wannaeat_Api/src/main/java/com/example/demo/WannaeatApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannaeatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannaeatApiApplication.class, args);
+	}
+
+}
